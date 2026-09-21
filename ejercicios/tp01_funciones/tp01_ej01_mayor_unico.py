f917ae15e0295f86d211bf0e10463b5ca44591aa
joelq20 <sanderqvallejos2020@gmail.com>
2026-09-21 12:11:32 -0300

    probando si funciona el commit

--- ejercicios/tp01_funciones/tp01_ej01_mayor_unico.py
+++ ejercicios/tp01_funciones/tp01_ej01_mayor_unico.py
@@ -1,1 +1,29 @@
-# Sander
+# Sander Quintanilla
+#Desarrollar una función que reciba tres números enteros positivos y devuelva el
+#mayor de los tres, sólo si éste es único (es decir el mayor estricto). Devolver -1 en
+#caso de no haber ninguno. No utilizar operadores lógicos (and, or, not). Desarrollar
+#también un programa para ingresar los tres valores, invocar a la función y mostrar
+#el máximo hallado, o un mensaje informativo si éste no existe.
+
+def mayor_unico(a, b, c):
+    if a > b:
+        if a > c:
+            return a
+    if b > a: 
+        if b > c:
+            return b
+    if c > a:
+        if c > b:
+            return c
+    return -1
+
+
+a=int(input("Ingrese el primer número entero positivo: "))
+b=int(input("Ingrese el segundo número entero positivo: "))
+c=int(input("Ingrese el tercer número entero positivo: "))
+
+if mayor_unico(a, b, c) != -1:
+    print("El mayor número único es:", mayor_unico(a, b, c))
+else:
+    print("No hay un mayor número único.")
+
---
+++ ejercicios/tp01_funciones/tp01_ej02_fecha_valida.py
@@ -0,0 +1,8 @@
+#Sander quintanilla 
+#Desarrollar una función que reciba tres números enteros positivos correspondientes
+#al día, mes, año de una fecha y verifique si corresponden a una fecha válida. Debe
+#enerse en cuenta la cantidad de días de cada mes, incluyendo los años bisiestos.
+#Devolver True o False según la fecha sea correcta o no. Realizar también un
+#programa para verificar el comportamiento de la función
+
+def fecha_valida(dia, mes ,anio):
